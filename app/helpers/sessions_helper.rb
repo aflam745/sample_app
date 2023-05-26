a765ffa5727c2ca6b41e24fcf7930523063639cb
Andrew Flammia <aflam745@gmail.com>
2023-05-26 13:49:56 -0400

    Finish user edit, update, index, and destroy actions

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -22,6 +22,10 @@ module SessionsHelper
         end
       end
     end
+
+    def current_user?(user)
+      user && user == current_user
+    end
   
     # Returns true if the user is logged in, false otherwise.
     def logged_in?
@@ -39,4 +43,16 @@ module SessionsHelper
       session.delete(:user_id)
       @current_user = nil
     end
+
+    def redirect_back_or(default)
+      redirect_to(session[:forwarding_url] || default)
+      session.delete(:forwarding_url)
+    end
+  
+    # Stores the URL trying to be accessed.
+    def store_location
+      session[:forwarding_url] = request.original_url if request.get?
+    end
+
+
   end
